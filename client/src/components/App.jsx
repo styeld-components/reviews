07926f05c51e64d9880e19f8680313bea2bf9424
Adrian <vent.adrian@gmail.com>
2020-06-22 13:23:51 -0700

    got Jest to finally run

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,12 +1,56 @@
 import React from 'react';
+import $ from 'jquery';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      value: 'neat'
+      reviews: [],
+      scores: null,
+      overall: null
     };
+  }
+
+  componentDidMount() {
+
+  }
+
+  // HTTP REQUESTS
+  getReviews(roomId) {
+    let url = `api/${roomId}/reviews`;
+    $.ajax({
+      type: 'GET',
+      url: url,
+      contentType: 'application/json',
+      success: data => {
+        this.setState({ reviews: data });
+      }
+    });
+  }
+
+  getReviewScores(roomId) {
+    let url = `api/${roomId}/reviews/scores`;
+    $.ajax({
+      type: 'GET',
+      url: url,
+      contentType: 'application/json',
+      success: data => {
+        this.setState({ scores: data });
+      }
+    });
+  }
+
+  getReviewOverall(roomId) {
+    let url = `api/${roomId}/reviews/overall`;
+    $.ajax({
+      type: 'GET',
+      url: url,
+      contentType: 'application/json',
+      success: data => {
+        this.setState({ overall: data });
+      }
+    });
   }
 
   render() {
