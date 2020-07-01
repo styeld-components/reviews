01a88de76a7a31d259b2d2a7f529654ee6eb25f9
Adrian <vent.adrian@gmail.com>
2020-06-30 18:36:03 -0700

    updated for proxy

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const controller = require('./controller.js');
 
 const app = express();
 const port = 3002;
 
+app.use(cors());
 app.use('/reviews', express.static(path.join(__dirname, '../client/dist')));
 app.use(bodyParser.json());
 
